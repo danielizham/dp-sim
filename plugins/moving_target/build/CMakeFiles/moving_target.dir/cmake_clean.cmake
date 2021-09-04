d7f18e923e14c1f6abc859b6e09ea826607aad81
Daniel Izham <daniel.izham@gmail.com>
2021-09-04 20:18:29 +0300

    Create initial worlds, models and plugins for Sphinx

---
+++ plugins/moving_target/build/CMakeFiles/moving_target.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moving_target.dir/moving_target.cc.o"
+  "libmoving_target.pdb"
+  "libmoving_target.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moving_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
