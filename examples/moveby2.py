4b09423ef45e680aea3051faa2e42610f5f3bd4a
Daniel Izham <daniel.izham@gmail.com>
2022-04-14 16:15:28 +0300

    Edit moveBy2.py to work with real drone and move a shorter distance

--- examples/moveby2.py
+++ examples/moveby2.py
@@ -3,7 +3,7 @@ import os
 from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing
 from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
 
-DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
 
 def test_moveby2():
@@ -14,7 +14,7 @@ def test_moveby2():
         >> FlyingStateChanged(state="hovering", _timeout=5)
     ).wait().success()
     assert drone(
-        moveBy(10, 0, 0, 0)
+        moveBy(3, 0, 0, 0)
         >> FlyingStateChanged(state="hovering", _timeout=5)
     ).wait().success()
     assert drone(Landing()).wait().success()
